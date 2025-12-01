6f7350cd23e6238e5b9fef89720e0a2b5430ad1d
prathamrpatel <rprathampatel@gmail.com>
2025-12-01 13:50:18 -0500

    initial MVP

--- agent.py
+++ agent.py
@@ -3,14 +3,31 @@ from autogen_ext.models.openai import OpenAIChatCompletionClient
 from autogen_ext.tools.mcp import mcp_server_tools, StdioServerParams
 from autogen_agentchat.teams import RoundRobinGroupChat
 from autogen_agentchat.conditions import FunctionCallTermination
+from autogen_core.model_context import UnboundedChatCompletionContext
+from autogen_core._types import FunctionCall
+from autogen_core.models._types import FunctionExecutionResult
 import asyncio
+from copy import deepcopy
+
+class MHContext(UnboundedChatCompletionContext):
+    async def get_messages(self):
+        message = await super().get_messages()
+        result = deepcopy(message)
+
+        indices_func_call = [i for i, msg in enumerate(message) if isinstance(msg.content, list) and isinstance(msg.content[0], FunctionCall)]
+        indices_func_result = [i for i, msg in enumerate(message) if isinstance(msg.content, list) and isinstance(msg.content[0], FunctionExecutionResult)]
+        indices = sorted(indices_func_call[:-1] + indices_func_result[:-1])
+
+        for i in reversed(indices):
+            result.pop(i)
+
+        return result
 
 def terminate():
     """Terminate the conversation"""
     pass
 
-
-async def main():
+async def config():
     params = StdioServerParams(
         command="npx",
         args=["-y", "mcp-remote", f"https://mcp.tavily.com/mcp/?tavilyApiKey={open('api_tavily.txt').read().strip()}"]
@@ -28,7 +45,8 @@ async def main():
         system_message=open('agent.txt').read().strip(),
         model_client=model,
         tools=(mcp_tools + [terminate]),
-        reflect_on_tool_use=True
+        reflect_on_tool_use=True,
+        model_context=MHContext()
     )
 
     team = RoundRobinGroupChat(
@@ -37,10 +55,17 @@ async def main():
         termination_condition=FunctionCallTermination(function_name='terminate')
     )
 
-    task = "Is Lionel Messi 25 years old?"
+    return team
 
+async def orchestrate(team, task):
     async for msg in team.run_stream(task=task):
-        print("---" * 20)
+        yield msg
+
+async def main():
+    team = await config()
+    task = "Is Lionel Messi 25 years old on 2025?"
+    async for msg in orchestrate(team, task):
+        print('---' * 25)
         print(msg)
 
 if __name__ == '__main__':
---
+++ app.py
@@ -0,0 +1,35 @@
+import streamlit as st
+from agent import orchestrate, config
+from autogen_agentchat.base import TaskResult
+from autogen_agentchat.messages import TextMessage
+from autogen_agentchat.messages import ToolCallExecutionEvent, ToolCallRequestEvent
+import asyncio
+
+st.title("📰 News Fact-Checking Agent 🤖✅")
+
+chat = st.container()
+task = st.chat_input("Enter your question about news or facts:")
+
+if task:
+    team = asyncio.run(config())
+    async def run_agent(team, task):
+        with chat:
+            async for msg in orchestrate(team, task):
+                print('---' * 20)
+                print(type(msg))
+                print(msg)
+                if not isinstance(msg, TaskResult):
+                    if msg.source == "user":
+                        with st.chat_message("user"):
+                            st.markdown(msg.content)
+                    elif msg.source == "agent":
+                        if isinstance(msg, ToolCallRequestEvent) or isinstance(msg, ToolCallExecutionEvent):
+                            with st.expander("Tool Call"):
+                                st.markdown(msg.content)
+                        else:
+                            with st.chat_message("assistant"):
+                                st.markdown(msg.content)
+
+    with st.spinner("Processing..."):
+        asyncio.run(run_agent(team, task))
+    st.success("Task completed successfully!")      
